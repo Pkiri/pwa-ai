3a212ab3bec34c3deb8b9eda234bae5741de887a
Pkiri <stuurhiermaarheen@outlook.com>
2021-01-28 22:33:17 +0100

    Adding Service worker with application insights

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,10 +4,13 @@ module.exports = {
     public: {url: '/', static: true},
     src: {url: '/dist'},
   },
-  plugins: ['@snowpack/plugin-typescript'],
+  plugins: [
+    ['@snowpack/plugin-typescript', { args: '--build src/main --verbose' }]
+  ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: 'all', src: '/worker.js', dest: '/dist/workers/worker.js' },
   ],
   optimize: {
     /* Example: Bundle your final build: */
--- src/main/index.ts
+++ src/main/index.ts
@@ -9,3 +9,6 @@ confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
   resize: true,
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
+
+navigator.serviceWorker.register('/worker.js');
+
